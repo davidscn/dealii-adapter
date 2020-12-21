a2faee05011d8b7f35d4da852ca88fba42aaa345
David Schneider <dav.schneider@tum.de>
2020-12-21 19:31:06 +0100

    Pull out PreciceCouplingConfiguration in a separate header file

--- linear_elasticity/include/parameter_handling.h
+++ linear_elasticity/include/parameter_handling.h
@@ -3,10 +3,12 @@
 
 #include <deal.II/base/parameter_handler.h>
 
+#include "../../adapter/precice_parameter.h"
+
 namespace Linear_Elasticity
 {
   using namespace dealii;
-
+  using namespace Parameters;
   /**
    * This class declares all parameters, which can be specified in the parameter
    * file. The subsection abut preCICE configurations is directly interlinked
@@ -206,87 +208,6 @@ namespace Linear_Elasticity
         type_lin           = prm.get("Solver type");
         tol_lin            = prm.get_double("Residual");
         max_iterations_lin = prm.get_double("Max iteration multiplier");
-      }
-      prm.leave_subsection();
-    }
-
-    /**
-     * @brief PreciceAdapterConfiguration: Specifies preCICE related information.
-     *        A lot of these information need to be consistent with the
-     *        precice-config.xml file.
-     */
-    struct PreciceAdapterConfiguration
-    {
-      std::string scenario;
-      std::string config_file;
-      std::string participant_name;
-      std::string read_mesh_name;
-      std::string write_mesh_name;
-      std::string read_data_name;
-      std::string write_data_name;
-
-      static void
-      declare_parameters(ParameterHandler &prm);
-
-      void
-      parse_parameters(ParameterHandler &prm);
-    };
-
-
-    void
-    PreciceAdapterConfiguration::declare_parameters(ParameterHandler &prm)
-    {
-      prm.enter_subsection("precice configuration");
-      {
-        prm.declare_entry("Scenario",
-                          "FSI3",
-                          Patterns::Selection("FSI3|PF"),
-                          "Cases: FSI3 or PF for perpendicular flap");
-        prm.declare_entry("precice config-file",
-                          "precice-config.xml",
-                          Patterns::Anything(),
-                          "Name of the precice configuration file");
-        prm.declare_entry(
-          "Participant name",
-          "dealiisolver",
-          Patterns::Anything(),
-          "Name of the participant in the precice-config.xml file");
-        prm.declare_entry(
-          "Read mesh name",
-          "read-mesh",
-          Patterns::Anything(),
-          "Name of the read coupling mesh in the precice-config.xml file");
-        prm.declare_entry(
-          "Write mesh name",
-          "write-mesh",
-          Patterns::Anything(),
-          "Name of the write coupling mesh in the precice-config.xml file");
-        prm.declare_entry(
-          "Read data name",
-          "received-data",
-          Patterns::Anything(),
-          "Name of the read data in the precice-config.xml file");
-        prm.declare_entry(
-          "Write data name",
-          "calculated-data",
-          Patterns::Anything(),
-          "Name of the write data in the precice-config.xml file");
-      }
-      prm.leave_subsection();
-    }
-
-    void
-    PreciceAdapterConfiguration::parse_parameters(ParameterHandler &prm)
-    {
-      prm.enter_subsection("precice configuration");
-      {
-        scenario         = prm.get("Scenario");
-        config_file      = prm.get("precice config-file");
-        participant_name = prm.get("Participant name");
-        read_mesh_name   = prm.get("Read mesh name");
-        write_mesh_name  = prm.get("Write mesh name");
-        read_data_name   = prm.get("Read data name");
-        write_data_name  = prm.get("Write data name");
       }
       prm.leave_subsection();
     }
